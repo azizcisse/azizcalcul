4813a1a48397cfaceb9310ac212d88c0a4c65d08
azizcisse <alaziz6c78@gmail.com>
2023-02-21 18:29:07 +0000

    Le projet

---
+++ android/app/src/main/kotlin/com/example/azizcalcul/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.azizcalcul
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color whiteColor = Colors.teal;
+
+const Color grayColor = Colors.grey;
+
+const Color blueColor = Colors.blue;
+
+const headingTextStyle = TextStyle(
+  color: Colors.white,
+);
